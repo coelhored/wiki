9a237ee289c2e12fd1cf7a71270a60b61937a81c
Cultch-DS <74624977+Cultch-DS@users.noreply.github.com>
2021-01-08 16:16:15 -0800

    initial commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    if util.get_entry(title):
+        return render(request, "encyclopedia/entry.html", {
+            "entry": util.get_entry(title),
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
